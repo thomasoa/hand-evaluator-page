fe945a7c0d0b846e3f51c825fbde47f10cd0fcac
Thomas Andrews <github@thomasoandrews.com>
2017-05-10 16:06:41 -0400

    Adding application model classes

---
+++ js/app.model.js
@@ -0,0 +1,35 @@
+InvalidHolding ={
+    'length': -100
+}
+
+function HandModel(deck) {
+    this.deck = deck;
+    var voidH = deck.h('-');
+    var _holdings = [voidH,voidH,voidH,voidH];
+    var _texts = ["","","",""];
+    this.length = 0;
+    this.holdings = function() {
+       return _holdings
+    }
+ 
+    this.clear = function() {
+	_holdings = [voidH,voidH,voidH,voidH];
+        this.length = 0;
+    }
+       
+    this.setHolding=function(suitString,hString) {
+        var suit = this.deck.suits.lookup(suitString);
+        hString = hString.toUpperCase();
+        if (hString != _texts[suit.index]) {
+            var oldHolding = _holdings[suit.index];
+            var holding = this.deck.h(hString);
+            if (holding == null) {
+		holding = InvalidHolding;
+	    }
+	    _holdings[suit.index]=holding;
+	    this.length = this.length-oldHolding.length + holding.length;
+	}
+	return _holdings[suit.index].length>=0;
+    } 
+
+}
